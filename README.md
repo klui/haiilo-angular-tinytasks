a64ce02db260f86a8642dfd08f0d0d939b7795a5
klui <klui@gmx.de>
2022-05-13 14:23:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# haiilo-angular-tinytasks
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/haiilo-angular-tinytasks)
---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import {
+  Component,
+  VERSION,
+  ChangeDetectionStrategy,
+  OnInit,
+  Inject,
+} from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent implements OnInit {
+  tasks$: Observable<Task[]>;
+
+  constructor(@Inject('TaskService') private taskService: TaskService) {
+    this.tasks$ = this.taskService.getAll();
+  }
+
+  ngOnInit(): void {
+    this.tasks$ = this.taskService.getAll();
+  }
+
+  created(): void {
+    this.tasks$ = this.taskService.getAll();
+  }
+
+  deleted(): void {
+    this.tasks$ = this.taskService.getAll();
+  }
+}
